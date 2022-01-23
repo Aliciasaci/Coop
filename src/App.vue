7cc92a1332aad4324cd9a19e3a185307bf00630e
alicia <aliciasaci@gmail.com>
2022-01-23 20:35:50 +0100

    v1

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+      <router-view/>  
+</template>
+<script>
+export default{
+  name : "app",
+  mounted(){
+    this.$api.get('ping').then();
+    if(!this.$store.state.token){
+      this.$router.push('login');
+    }
+  }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/',
+        name: 'Conversations',
+        component: () =>
+            import ('../views/Conversations.vue')
+    },
+    {
+        path: '/SingnIn',
+        name: 'SingnIn',
+        component: () =>
+            import ('../views/SingnIn.vue')
+    },
+    {
+        path: '/Login',
+        name: 'Login',
+        component: () =>
+            import ('../views/Login.vue')
+    },
+    {
+        path: '/createConversation',
+        name: '/createConversation',
+        component: () =>
+            import ('../views/createConversation.vue')
+    },
+    {
+        path: '/Conversation/:id',
+        name: 'Conversation',
+        component: () =>
+            import ('../views/Conversation.vue')
+    },
+    {
+        path: '/deleteConversation/:id',
+        name: 'deleteConversation',
+        component: () =>
+            import ('../views/deleteConversation.vue')
+    },
+    {
+        path: '/logOut',
+        name: '/logOut',
+        component: () =>
+            import ('../views/logOut.vue')
+    },
+    {
+        path: '/Members',
+        name: 'Members',
+        component: () =>
+            import ('../views/Members')
+    },
+]
+
+const router = new VueRouter({
+    routes
+})
+
+export default router
